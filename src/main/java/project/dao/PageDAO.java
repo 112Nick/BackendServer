351f6e9c3cd2425c3d13be68f31dc15a2887ec07
112Nick <mrnickrus@gmail.com>
2018-10-14 22:02:00 +0300

    template fix

--- src/main/java/project/dao/PageDAO.java
+++ src/main/java/project/dao/PageDAO.java
@@ -183,12 +183,12 @@ public class PageDAO {
 
         switch(own) {
             case "me":
-                sqlQuery = "SELECT uuid, ownerid, title, ispublic, isstatic, fieldsnames, fieldsvalues, date, " +
+                sqlQuery = "SELECT uuid, ownerid, title, ispublic, isstatic, template, plAfieldsnames, fieldsvalues, date, " +
                         "CASE WHEN ownerid = ? THEN true ELSE false END AS ismine " +
                         "FROM page WHERE ownerid = ?";
                 break;
             case "others":
-                sqlQuery = "SELECT uuid, ownerid, title, ispublic, isstatic, fieldsnames, fieldsvalues, date, " +
+                sqlQuery = "SELECT uuid, ownerid, title, ispublic, isstatic, template, fieldsnames, fieldsvalues, date, " +
                         "CASE WHEN ownerid = ? THEN true ELSE false END AS ismine " +
                         "FROM userpages JOIN page ON pageuuid = uuid WHERE userid = ? ";
 
@@ -196,20 +196,20 @@ public class PageDAO {
             case "all":
                 tmpObj.add(userID);
                 tmpObj.add(userID);
-                sqlQuery = "SELECT * FROM (SELECT uuid, ownerid, title, ispublic, isstatic, fieldsnames, fieldsvalues, date, " +
+                sqlQuery = "SELECT * FROM (SELECT uuid, ownerid, title, ispublic, template, isstatic, fieldsnames, fieldsvalues, date, " +
                         "CASE WHEN ownerid = ? THEN true ELSE false END AS ismine " +
                         "FROM page WHERE ownerid = ? " +
-                        "UNION SELECT uuid, ownerid, title, ispublic, isstatic, fieldsnames, fieldsvalues, date, " +
+                        "UNION SELECT uuid, ownerid, title, ispublic, isstatic,template, fieldsnames, fieldsvalues, date, " +
                         "CASE WHEN ownerid = ? THEN true ELSE false END AS ismine " +
                         "FROM userpages JOIN page ON pageuuid = uuid WHERE userid = ?) as h";
                 break;
             default:
                 tmpObj.add(userID);
                 tmpObj.add(userID);
-                sqlQuery = "SELECT * FROM (SELECT uuid, ownerid, title, ispublic, isstatic, fieldsnames, fieldsvalues, date, " +
+                sqlQuery = "SELECT * FROM (SELECT uuid, ownerid, title, ispublic, isstatic, template, fieldsnames, fieldsvalues, date, " +
                         "CASE WHEN ownerid = ? THEN true ELSE false END AS ismine  " +
                         "FROM page WHERE ownerid = ? " +
-                        "UNION SELECT uuid, ownerid, title, ispublic, isstatic, fieldsnames, fieldsvalues, date, " +
+                        "UNION SELECT uuid, ownerid, title, ispublic, isstatic, template, fieldsnames, fieldsvalues, date, " +
                         "CASE WHEN ownerid = ? THEN true ELSE false END AS ismine " +
                         "FROM userpages JOIN page ON pageuuid = uuid WHERE userid = ?) as h";
                 break;
